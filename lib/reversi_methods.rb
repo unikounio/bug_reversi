ae52f1b1e52faa74bd86c9b858b0234bcf7bdc03
unikounio <bubbbble428@gmail.com>
2023-08-02 16:44:48 +0900

    すべてのテストを通過しrubocopを通した

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -62,7 +62,7 @@ module ReversiMethods
 
   def turn(board, target_pos, attack_stone_color, direction)
     return false if target_pos.out_of_board?
-    return false if target_pos.stone_color(board) == attack_stone_color
+    return false if target_pos.stone_color(board) == attack_stone_color || target_pos.stone_color(board) == BLANK_CELL
 
     next_pos = target_pos.next_position(direction)
     if (next_pos.stone_color(board) == attack_stone_color) || turn(board, next_pos, attack_stone_color, direction)
@@ -74,7 +74,6 @@ module ReversiMethods
   end
 
   def finished?(board)
-    return true if count_stone(board, WHITE_STONE) == 0 || count_stone(board, BLACK_STONE) == 0
     !placeable?(board, WHITE_STONE) && !placeable?(board, BLACK_STONE)
   end
 
@@ -87,8 +86,7 @@ module ReversiMethods
         return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
-
-
+    false
   end
 
   def count_stone(board, stone_color)
